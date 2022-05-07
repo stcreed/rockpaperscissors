c2e9674874139b4bfd532b596975342780febcee
Sam Creed <sam_creed@hotmail.co.uk>
2022-05-07 23:53:15 +0100

    finalise files for game

--- index.js
+++ index.js
@@ -1,8 +1,46 @@
 //jshint esversion:6
 
-const playerSelection = (prompt("Insert choice here: ").toLowerCase());
+const choices = ["rock", "paper", "scissors"];
+let winners = [];
 
-function computerPlay() {
+function game() {
+  for (let i = 1; i <= 5; i++) {
+    playRound(i);
+    }
+  logWins();
+}
+
+function playRound(round) {
+  const playerSelection = playerChoice();
+  const computerSelection = computerChoice();
+  const winner = checkWinner(playerSelection, computerSelection);
+  winners.push(winner);
+  logRound(playerSelection, computerSelection, winner, round);
+}
+
+function playerChoice() {
+  let input = prompt("Type rock, paper or scissors:");
+  while (input == null) {
+    input = prompt("Type rock, paper or scissors:");
+  }
+  input = input.toLowerCase();
+  let check = validateInput(input);
+  while (check == false) {
+    input = prompt(
+      "Type rock, paper or scissors. Spelling needs to be exact but capitalisation doesn't matter"
+    );
+    while (input == null) {
+      input = prompt("Type rock, paper or scissors:");
+    }
+  }
+  return input;
+}
+
+function validateInput(choice){
+  return choices.includes(choice);
+}
+
+function computerChoice() {
   const compNum = Math.floor(Math.random() * 3) + 1;
   if (compNum == 1) {
     return "rock";
@@ -13,22 +51,32 @@ function computerPlay() {
   }
 }
 
-const computerSelection = computerPlay();
-
-function playRound(playerSelection, computerSelection) {
+function checkWinner(playerSelection, computerSelection) {
   if (playerSelection == computerSelection) {
     return "Draw";
-  } else if ((playerSelection == "rock" && computerSelection == "Paper") || (playerSelection == "paper" && computerSelection == "scissors") || (playerSelection == "scissors" && computerSelection == "rock")) {
+  } else if (
+    (playerSelection == "rock" && computerSelection == "paper") ||
+    (playerSelection == "paper" && computerSelection == "scissors") ||
+    (playerSelection == "scissors" && computerSelection == "rock")) {
     return "You lose";
-  } else if ((playerSelection == "Paper" && computerSelection == "rock") || (playerSelection == "scissors" && computerSelection == "paper") || (playerSelection == "rock" && computerSelection == "scissors")) {
+  } else{
     return "You win";
   }
 }
 
+function logWins(){
+  let playerWins = winners.filter((item) => item == "You win").length;
+  let compWins = winners.filter((item) => item == "You lose").length;
+  let ties = winners.filter((item) => item == "Draw").length;
+  console.log("Results:");
+  console.log("Player wins: ",playerWins);
+  console.log("Computer wins: ",compWins);
+  console.log("Ties: ",ties);
+}
 
-
-console.log(computerSelection);
-
-console.log(playerSelection);
-
-console.log(playRound(playerSelection, computerSelection));
+function logRound(playerChoice,computerChoice,winner,round){
+  console.log("Round: ",round);
+  console.log("Player chose: ",playerChoice);
+  console.log("Computer Chose: ",computerChoice);
+  console.log(winner);
+}
